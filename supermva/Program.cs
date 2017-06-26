7d08be4e6db3fda0dab398d41eca07f66b488429
Phayao Boonon <phayaob@gmail.com>
2017-06-26 22:46:35 +0700

    Gettng Started with ASP.NET Core

---
+++ supermva/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using movingtotheweb;
+namespace supermva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
