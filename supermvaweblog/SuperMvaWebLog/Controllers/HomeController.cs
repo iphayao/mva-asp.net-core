5e1977a0e4b72deab84e6645109c0e343e4095f0
Phayao Boonon <phayaob@gmail.com>
2017-06-30 23:21:57 +0700

    Logging and Diagnostics

---
+++ supermvaweblog/SuperMvaWebLog/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SuperMvaWebLog.Controllers
+{
+    public class HomeController : Controller
+    {
+        ILogger _log;
+
+        public HomeController(ILogger<HomeController> log)
+        {
+            _log = log;
+        }
+
+        public IActionResult Index()
+        {
+            _log.LogCritical("SOMETHING BAD");
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+            _log.LogInformation("We are in About");
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
