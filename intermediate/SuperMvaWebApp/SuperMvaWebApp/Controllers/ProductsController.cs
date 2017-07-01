bcd2daa34dc0907f4851b19a6665b629cd6cd246
Phayao Boonon <phayaob@gmail.com>
2017-07-01 23:37:53 +0700

    APIs and MVC Core

---
+++ intermediate/SuperMvaWebApp/SuperMvaWebApp/Controllers/ProductsController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SuperMvaWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperMvaWebApp.Controllers
+{
+    [Route("/api/[controller]")]
+    [Produces("application/json")]
+    public class ProductsController : ControllerBase
+    {
+        private static List<Product> _products = new List<Product>(new[]
+        {
+            new Product() { Id = 1, Name = "Computer" },
+            new Product() { Id = 2, Name = "Radio" },
+            new Product() { Id = 3, Name = "Apple" },
+            new Product() { Id = 4, Name = "Pants" },
+            new Product() { Id = 5, Name = "Tacos" }
+        });
+
+        [HttpGet]
+        public IEnumerable<Product> Get() => _products;
+
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var product = _products.SingleOrDefault(p => p.Id == id);
+
+            if(product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+        
+        [HttpPost]
+        public IActionResult Post([FromBody]Product product)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            _products.Add(product);
+            return CreatedAtAction(nameof(Get), new { id = product.Id }, product);
+        }
+
+    }
+}
--- intermediate/SuperMvaWebApp/SuperMvaWebApp/Startup.cs
+++ intermediate/SuperMvaWebApp/SuperMvaWebApp/Startup.cs
@@ -29,7 +29,8 @@ namespace SuperMvaWebApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc().AddXmlDataContractSerializerFormatters();
+            
             services.AddSingleton<IRequestIdFactory, RequestIdFactory>();
             services.AddScoped<IRequestId, RequestId>();
         }
